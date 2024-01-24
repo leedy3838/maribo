d834702ccefa3dd8690814be7df620331cbe0c53
Amepistheo <kellylhji@gmail.com>
2024-01-24 19:42:05 +0900

    feat : 게시글 수정 request 생성 #1

--- maribo_BE/src/main/java/project/maribo/domain/dto/PostUpdateRequest.java
+++ maribo_BE/src/main/java/project/maribo/domain/dto/PostUpdateRequest.java
@@ -4,4 +4,10 @@ import lombok.Data;
 
 @Data
 public class PostUpdateRequest {
+    private Long userId;
+    private Long postId;
+    private String title;
+    private String content;
+    private String photoUrl;
+    private String category;
 }
