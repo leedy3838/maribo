0dd4b15626d34857cba265ea3811c5d6a28d0140
Amepistheo <kellylhji@gmail.com>
2024-01-24 16:07:32 +0900

    feat : 카테고리 문자열 입력시 enum 타입으로 변경 (default는 normal) #1

--- maribo_BE/src/main/java/project/maribo/domain/entity/type/Category.java
+++ maribo_BE/src/main/java/project/maribo/domain/entity/type/Category.java
@@ -1,5 +1,13 @@
 package project.maribo.domain.entity.type;
 
 public enum Category {
-    INFORMATION, QUESTION, ADVERTISE
+    INFORMATION, QUESTION, ADVERTISE, NORMAL
+
+    public static Category of (String category) {
+        if (category.equalsIgnoreCase("information")) return Category.INFORMATION;
+        else if (category.equalsIgnoreCase("question")) return Category.QUESTION;
+        else if (category.equalsIgnoreCase("advertise")) return Category.ADVERTISE;
+        else if (category.equalsIgnoreCase("normal")) return Category.NORMAL;
+        else return Category.NORMAL;
+    }
 }
