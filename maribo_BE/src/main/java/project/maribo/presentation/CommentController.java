676855dd71adb071580c122f4a15db3c9187242c
Amepistheo <kellylhji@gmail.com>
2024-02-17 22:28:39 +0900

    feat : 댓글 ApiResponse 추가 #8

--- maribo_BE/src/main/java/project/maribo/presentation/CommentController.java
+++ maribo_BE/src/main/java/project/maribo/presentation/CommentController.java
@@ -1,6 +1,10 @@
 package project.maribo.presentation;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +12,8 @@ import org.springframework.web.bind.annotation.*;
 import project.maribo.application.CommentService;
 import project.maribo.domain.dto.CommentRequest;
 import project.maribo.domain.dto.CommentDeleteRequest;
+import project.maribo.domain.dto.PostResponse;
+import project.maribo.domain.dto.UserRequest;
 
 @Tag(name = "Comment", description = "댓글 API")
 @RestController
@@ -17,6 +23,8 @@ public class CommentController {
 
     private final CommentService commentService;
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "댓글 등록 성공", content = @Content(schema = @Schema(implementation = CommentRequest.class)))})
     @Operation(summary = "댓글 등록", description = "댓글 등록 API")
     @PostMapping("/{postId}")
     public ResponseEntity<Void> createComment(
@@ -26,6 +34,8 @@ public class CommentController {
         return ResponseEntity.ok().build();
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "댓글 수정 성공", content = @Content(schema = @Schema(implementation = CommentRequest.class)))})
     @Operation(summary = "댓글 수정", description = "댓글 수정 API")
     @PatchMapping("/{commentId}")
     public ResponseEntity<Void> updateComment(
@@ -35,6 +45,8 @@ public class CommentController {
         return ResponseEntity.ok().build();
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "댓글 삭제 성공", content = @Content(schema = @Schema(implementation = CommentDeleteRequest.class)))})
     @Operation(summary = "댓글 삭제", description = "댓글 삭제 API")
     @DeleteMapping("{commentId}")
     public ResponseEntity<Void> deleteComment(
