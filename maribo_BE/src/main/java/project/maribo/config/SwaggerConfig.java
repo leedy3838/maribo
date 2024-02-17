d228a85894099d6d77c6ecd84c835c76847999d2
Amepistheo <kellylhji@gmail.com>
2024-02-17 22:04:05 +0900

    fix : Swagger 3.0에서 사용하는 OpenAPI로 Swagger 오류 수정 #8

--- maribo_BE/src/main/java/project/maribo/config/SwaggerConfig.java
+++ maribo_BE/src/main/java/project/maribo/config/SwaggerConfig.java
@@ -1,37 +1,13 @@
 package project.maribo.config;
 
-import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
+@OpenAPIDefinition(
+        info = @Info(title = "Maribo API",
+                description = "게시판 서비스 API 명세서",
+                version = "1.0.0"))
 @Configuration
 public class SwaggerConfig {
-
-    private static final String API_NAME = "Maribo API";
-    private static final String API_VERSION = "0.0.1";
-    private static final String API_DESCRIPTION = "Maribo API Description";
-
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .useDefaultResponseMessages(false)
-                .apiInfo(apiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.project.maribo"))
-                .paths(PathSelectors.any())
-                .build();
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Maribo Swagger")
-                .description("스웨거 기능")
-                .version("1.0")
-                .build();
-    }
 }
