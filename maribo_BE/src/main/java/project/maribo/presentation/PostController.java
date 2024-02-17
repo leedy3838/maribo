d22e2caca68103a7619d51d51a51be7827378930
Amepistheo <kellylhji@gmail.com>
2024-02-17 22:32:26 +0900

    feat : 게시물 ApiResponse 추가 #8

--- maribo_BE/src/main/java/project/maribo/presentation/PostController.java
+++ maribo_BE/src/main/java/project/maribo/presentation/PostController.java
@@ -1,6 +1,10 @@
 package project.maribo.presentation;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,6 +24,8 @@ public class PostController {
 
     private final PostService postService;
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "게시물 등록 성공", content = @Content(schema = @Schema(implementation = PostCreateRequest.class)))})
     @Operation(summary = "게시물 등록", description = "게시물 등록 API")
     @PostMapping("/post")
     public ResponseEntity<Void> createPost(@RequestBody PostCreateRequest postCreateRequest) {
@@ -27,6 +33,8 @@ public class PostController {
         return ResponseEntity.ok().build();
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "게시물 수정 성공", content = @Content(schema = @Schema(implementation = PostUpdateRequest.class)))})
     @Operation(summary = "게시물 수정", description = "게시물 수정 API")
     @PostMapping("/modification")
     public ResponseEntity<Void> updatePost(@RequestBody PostUpdateRequest postUpdateRequest) {
@@ -34,6 +42,8 @@ public class PostController {
         return ResponseEntity.ok().build();
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "게시물 삭제 성공", content = @Content(schema = @Schema(implementation = PostDeleteRequest.class)))})
     @Operation(summary = "게시물 삭제", description = "게시물 삭제 API")
     @DeleteMapping("/{postId}")
     public ResponseEntity<Void> deletePost(
@@ -43,6 +53,8 @@ public class PostController {
         return ResponseEntity.noContent().build();
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "게시물 조회 성공", content = @Content(schema = @Schema(implementation = PostGetResponse.class)))})
     @Operation(summary = "게시물 조회", description = "게시물 조회 API")
     @GetMapping("/{postId}")
     public ResponseEntity<PostGetResponse> getPostById(@PathVariable Long postId) {
@@ -51,6 +63,8 @@ public class PostController {
                 .body(postService.getPostById(postId));
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "게시물 리스트 조회 성공", content = @Content(schema = @Schema(implementation = PostResponse.class)))})
     @Operation(summary = "게시물 리스트 조회", description = "게시물 리스트 조회 API")
     @GetMapping("/posts")
     public ResponseEntity<List<PostResponse>> getPosts() {
