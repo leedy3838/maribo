d2cb4ebfd0fa9272e3e88aef30a75f971e953b6b
Amepistheo <kellylhji@gmail.com>
2024-02-17 22:10:16 +0900

    feat : Swagger 게시판부분 구현 #8

--- maribo_BE/src/main/java/project/maribo/presentation/PostController.java
+++ maribo_BE/src/main/java/project/maribo/presentation/PostController.java
@@ -1,5 +1,7 @@
 package project.maribo.presentation;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -7,11 +9,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import project.maribo.application.PostService;
 import project.maribo.domain.dto.*;
-import project.maribo.domain.entity.Post;
-import project.maribo.repository.PostRepository;
 
 import java.util.List;
 
+@Tag(name = "Post", description = "게시물 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("v1/maribo/post")
@@ -19,18 +20,21 @@ public class PostController {
 
     private final PostService postService;
 
+    @Operation(summary = "게시물 등록", description = "게시물 등록 API")
     @PostMapping("/post")
     public ResponseEntity<Void> createPost(@RequestBody PostCreateRequest postCreateRequest) {
         postService.createPost(postCreateRequest);
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "게시물 수정", description = "게시물 수정 API")
     @PostMapping("/modification")
     public ResponseEntity<Void> updatePost(@RequestBody PostUpdateRequest postUpdateRequest) {
         postService.updatePost(postUpdateRequest);
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "게시물 삭제", description = "게시물 삭제 API")
     @DeleteMapping("/{postId}")
     public ResponseEntity<Void> deletePost(
             @PathVariable Long postId,
@@ -39,6 +43,7 @@ public class PostController {
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "게시물 조회", description = "게시물 조회 API")
     @GetMapping("/{postId}")
     public ResponseEntity<PostGetResponse> getPostById(@PathVariable Long postId) {
         return ResponseEntity
@@ -46,6 +51,7 @@ public class PostController {
                 .body(postService.getPostById(postId));
     }
 
+    @Operation(summary = "게시물 리스트 조회", description = "게시물 리스트 조회 API")
     @GetMapping("/posts")
     public ResponseEntity<List<PostResponse>> getPosts() {
         return ResponseEntity
